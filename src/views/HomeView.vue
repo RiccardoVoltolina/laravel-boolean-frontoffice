aa6f4e34602238d212ee28a2a8466187362b7c5f
RiccardoVoltolina <riky.volto@live.com>
2023-11-23 16:56:52 +0100

    fix initial page error and create a axios call function

---
+++ src/views/HomeView.vue
@@ -0,0 +1,70 @@
+<script>
+
+import axios from 'axios';
+
+
+
+
+export default {
+
+
+    name: 'HomeView',
+    data() {
+        return {
+
+            base_url: 'http://localhost:8000',
+
+            cocktails_url: '/api/cocktails',
+
+            cocktails: [],
+
+        }
+    },
+
+ 
+    mounted() {
+
+        // richiamo la funzione per la mia chiamata
+
+        this.axiosCall()
+
+    },
+
+    methods: {
+
+  
+
+        // eseguo la chiamata axios
+
+        axiosCall: function () {
+            axios.get(this.base_url + this.cocktails_url)
+                .then(response => {
+
+
+
+                    this.cocktails = response.data.result
+
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        }
+
+    },
+}
+</script>
+
+<template>
+    <div id="app">
+
+
+
+       
+
+
+    </div>
+</template>
+
+<style></style>
+
+
