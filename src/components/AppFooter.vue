05bf86fa202e2892e296e78b0ebb6e81ce69ee9a
RiccardoVoltolina <riky.volto@live.com>
2023-11-24 16:32:54 +0100

    modify footer layout

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,21 +5,21 @@ export default {
 </script>
 
 <template>
-    <footer class="bg-secondary py-3">
+    <footer class="bg-light py-3">
 
-        <div class="container text-start">
+        <div class="container text-center">
             <div class="row">
                 <div class="col-4">
                     <h4>Home</h4>
                     <ul class="list-unstyled">
                         <li>
-                            <a href="#" class="text-decoration-none text-white fw-medium fs-6">Home</a>    
+                            <a href="#" class="text-decoration-none text-black fw-medium fs-6">Home</a>    
                         </li>
                         <li>
-                            <a href="#" class="text-decoration-none text-white fw-medium fs-6">About</a>    
+                            <a href="#" class="text-decoration-none text-black fw-medium fs-6">About</a>    
                         </li>
                         <li>
-                            <a href="#" class="text-decoration-none text-white fw-medium fs-6">Contacts</a>    
+                            <a href="#" class="text-decoration-none text-black fw-medium fs-6">Contacts</a>    
                         </li>
                     </ul>
                 </div>
@@ -28,13 +28,13 @@ export default {
                     <h4>About</h4>
                     <ul class="list-unstyled">
                         <li>
-                            <a href="#" class="text-decoration-none text-white fw-medium fs-6">Drink</a>    
+                            <a href="#" class="text-decoration-none text-black fw-medium fs-6">Drink</a>    
                         </li>
                         <li>
-                            <a href="#" class="text-decoration-none text-white fw-medium fs-6">Team</a>    
+                            <a href="#" class="text-decoration-none text-black fw-medium fs-6">Team</a>    
                         </li>
                         <li>
-                            <a href="#" class="text-decoration-none text-white fw-medium fs-6">Coming Soon</a>    
+                            <a href="#" class="text-decoration-none text-black fw-medium fs-6">Coming Soon</a>    
                         </li>
                     </ul>
                 </div>
@@ -43,13 +43,13 @@ export default {
                     <h4>Contacts</h4>
                     <ul class="list-unstyled">
                         <li>
-                            <a href="#" class="text-decoration-none text-white fw-medium fs-6">Login</a>    
+                            <a href="#" class="text-decoration-none text-black fw-medium fs-6">Login</a>    
                         </li>
                         <li>
-                            <a href="#" class="text-decoration-none text-white fw-medium fs-6">Assistance</a>    
+                            <a href="#" class="text-decoration-none text-black fw-medium fs-6">Assistance</a>    
                         </li>
                         <li>
-                            <a href="#" class="text-decoration-none text-white fw-medium fs-6">Email</a>    
+                            <a href="#" class="text-decoration-none text-black fw-medium fs-6">Email</a>    
                         </li>
                     </ul>
                 </div>
